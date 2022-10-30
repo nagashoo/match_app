88c182df3f8418c09e1f9f2436ff4eb59f9139fb
johnmanjiro4th <johnmanjiro3rd@gmail.com>
2022-10-30 23:25:06 +0900

    不要なマイグレーションファイルの削除

--- db/migrate/20221024065126_remove_prifile_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemovePrifileFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :prifile, :text
-  end
-end
--- db/migrate/20221024065756_rename_profile_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameProfileColumnToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :profile, :text
-  end
-end
